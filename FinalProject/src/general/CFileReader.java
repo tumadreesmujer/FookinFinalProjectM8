8c923ed767902d9e077842746537ba0ab94a331d
jordanpkimball <jordanpkimball@gmail.com>
2017-05-22 18:13:34 -0700

    updated FileReader

--- FinalProject/src/general/CFileReader.java
+++ FinalProject/src/general/CFileReader.java
@@ -5,20 +5,22 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class CFileReader {
+	public static String[] s;
 	public CFileReader(File f) throws IOException{
 		readFile(f.getAbsolutePath());
 	}
-	public static String[] readFile(String path) throws IOException{
+	private static void readFile(String path) throws IOException{
 		FileReader fr;
 		try {
 			fr = new FileReader(path);
 		} catch (FileNotFoundException e) {
-			File temp = new File(path);
-			fr = new FileReader(temp);
-			e.printStackTrace();
+			PrintWriter pr = new PrintWriter(path, "UTF-8");
+			pr.close();
+			fr = new FileReader(path);
 		}
 		BufferedReader tr = new BufferedReader(fr);
 		ArrayList<String> f = new ArrayList<String>();
@@ -26,11 +28,22 @@ public class CFileReader {
 		while((temp = tr.readLine())!=null){
 			f.add(temp);
 		}
-		String[] tempArr = new String[f.size()];
+		s = new String[f.size()];
 		tr.close();
-		return f.toArray(tempArr);
+	}
+	public static String getLine(int i){
+		return s[i];
+	}
+	public static String[] getText(){
+		return s;
+	}
+	public static void updateLine(int i, String S){
+		s[i]=S;
+	}
+	public static void updateText(String[] S){
+		s=S;
 	}
 	public static void main(String[] args) throws IOException{
-		CFileReader f1 = new CFileReader(new File("C:/Users/jordanpk18/Documents/git/FookinFinalProjectM8/EZPZ.md"));
+		CFileReader f1 = new CFileReader(new File("C:/Users/Jordan/Documents/GitHub/FookinFinalProjectM8/ezpz.md"));
 	}
 }
