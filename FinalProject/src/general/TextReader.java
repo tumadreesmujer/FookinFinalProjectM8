c74f90da1ebe85db299e987dc0257ca180bde246
jordanpkimball <jordanpkimball@gmail.com>
2017-05-22 18:56:24 -0700

    added file saver

--- FinalProject/src/general/TextReader.java
+++ FinalProject/src/general/TextReader.java
@@ -8,9 +8,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-public class CFileReader {
-	public static String[] s;
-	public CFileReader(File f) throws IOException{
+public class TextReader {
+	public static ArrayList<String> s;
+	public static PrintWriter pr;
+	public TextReader(File f) throws IOException{
 		readFile(f.getAbsolutePath());
 	}
 	private static void readFile(String path) throws IOException{
@@ -18,7 +19,7 @@ public class CFileReader {
 		try {
 			fr = new FileReader(path);
 		} catch (FileNotFoundException e) {
-			PrintWriter pr = new PrintWriter(path, "UTF-8");
+			pr = new PrintWriter(path, "UTF-8");
 			pr.close();
 			fr = new FileReader(path);
 		}
@@ -28,22 +29,26 @@ public class CFileReader {
 		while((temp = tr.readLine())!=null){
 			f.add(temp);
 		}
-		s = new String[f.size()];
 		tr.close();
 	}
 	public static String getLine(int i){
-		return s[i];
+		return s.get(i);
 	}
-	public static String[] getText(){
+	public static ArrayList<String> getText(){
 		return s;
 	}
 	public static void updateLine(int i, String S){
-		s[i]=S;
+		s.set(i, S);
 	}
-	public static void updateText(String[] S){
-		s=S;
+	public static void updateText(ArrayList<String> S){
+		s=S;	
+	}
+	public static void saveText(){
+		for(int i=0;i<s.size();i++)
+			pr.println(s.get(i));
+		pr.close();
 	}
 	public static void main(String[] args) throws IOException{
-		CFileReader f1 = new CFileReader(new File("C:/Users/Jordan/Documents/GitHub/FookinFinalProjectM8/ezpz.md"));
+		TextReader f1 = new TextReader(new File("C:/Users/Jordan/Documents/GitHub/FookinFinalProjectM8/ezpz.md"));
 	}
 }
