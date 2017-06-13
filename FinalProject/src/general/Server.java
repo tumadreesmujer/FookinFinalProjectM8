f65e443b8b46bb764def9af8f30f1411bfde781e
Jordan Kimball <jkimball@trcsolutions.com>
2017-06-12 23:23:23 -0700

    edited server code

--- FinalProject/src/general/Server.java
+++ FinalProject/src/general/Server.java
@@ -1,14 +1,17 @@
 package general;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A TCP server that runs on port 9090.  When a client connects, it
@@ -21,30 +24,36 @@ public class Server {
 	static Socket socket;
     /**
      * Runs the server.
+     * @throws InterruptedException 
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         listener = new ServerSocket(9090);
         try {
             while (true) {
                 socket = listener.accept();
                 try {
-                    PrintWriter out =
-                        new PrintWriter(socket.getOutputStream(), true);
-                    System.out.println("Connected to:"+socket.getInetAddress()+":"+socket.getPort());
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    System.out.println("Connected to:"+socket.getInetAddress().toString().substring(1)+":"+socket.getPort());
                     out.println(new Date().toString());
-                    File tFile=new File("res/test/maxresdefault.jpg");
-                    byte[] tByte=new byte[1024*3];
-                    
-                    
+                    out.flush();
+                    //sendFile(new File("res/test/code.jpg"));
+                    File tFile=new File("res/test/code.png");/*
+                    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    System.out.println(in.readLine());
+                    in.close();*/
+                    int B=tFile.length()<1024*64?(int)tFile.length():1024*64;
+                    byte[] tByte=new byte[B];
                     BufferedInputStream bis = new BufferedInputStream(new FileInputStream(tFile));
                     OutputStream os = socket.getOutputStream();
-                    bis.read(tByte, 0, 1024);
-                    os.write(tByte, 0, 1024);
-                    //os.flush();
-                    bis.read(tByte, 1024, 1024);
-                    os.write(tByte, 0, 1024);
-                    bis.read(tByte, 2048, 1024);
-                    os.write(tByte, 0, 1024);
+                    PrintWriter pr = new PrintWriter(socket.getOutputStream());
+                    pr.println("testcode.png");
+                    pr.flush();/*
+                    in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    System.out.println(in.readLine());*/
+                    pr.close();
+                    bis.read(tByte, 0, B);
+                    os.write(tByte, 0, B);
                     os.flush();
                     bis.close();
                     
@@ -56,6 +65,25 @@ public class Server {
         finally {
             
         }
+    }
+    public static void sendFile(File f) throws IOException{
+            PrintWriter out =
+                new PrintWriter(socket.getOutputStream(), true);
+            System.out.println("Connected to "+socket.getInetAddress().toString().substring(1)+":"+socket.getPort());
+            out.println(new Date().toString());
+            
+            int B=f.length()<1024*64?(int)f.length():1024*64;
+            
+            byte[] tByte=new byte[B];
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(f));
+            OutputStream os = socket.getOutputStream();
+            bis.read(tByte, 0, B);
+            os.write(tByte, 0, B);
+            os.flush();
+            bis.close();
+            
+            
+        
     }
     public void killServer() throws IOException{
             socket.close();
